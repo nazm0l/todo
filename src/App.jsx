bf42dfed721526fbd487d69afe9b9532a4e07583
nazm0l <tanjirahmednazmul@gmail.com>
2025-08-30 03:02:34 +0600

    Local storage functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useContext, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import HeaderStats from "./components/HeaderStats";
 import Todos from "./components/Todos";
+import { TodoContext } from "./context/TodoContext";
 
 function App() {
+  const { state, dispatch } = useContext(TodoContext);
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      savedTodos.forEach((todo) => {
+        dispatch({ type: "ADD", payload: todo });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(state.todos));
+  }, [state.todos]);
+
   return (
     <>
       <section className="container mx-auto p-4">
